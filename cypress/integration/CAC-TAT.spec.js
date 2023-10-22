6d827b404128c293f38f6762dcfa55a73ccec90c
victorsantos274 <victorviolinista274@gmail.com>
2023-10-22 17:20:53 -0300

    test case to validate typing errors in the email and telephone fields

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,7 +10,7 @@ cy.visit('./src/index.html')
 
 cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
     })
-    it.only('preencha os campos obrigatórios e envia o formulário', function() {
+    it('preencha os campos obrigatórios e envia o formulário', function() {
 const longText = 'teste teste teste teste teste teste teste'
 
 cy.get('#firstName').type('Victor')
@@ -28,5 +28,25 @@ cy.get('button[type="submit"]')
 cy.get('.success').should('be.visible')
 
     })
+
+    it('exibe mensagem de erro ao submiter e-mail com formatação inválida,', function(){
+        cy.get('#firstName').type('Victor')
+        cy.get('#lastName').type('santos')
+        cy.get('#email').type('victor,gmail.com')
+        cy.get('#open-text-area').type('testes')
+        cy.get('button[type="submit').click()
+
+        cy.get('.error').should('be.visible')
+    }
+    
+    )
+
+it.only('campo telefone ainda continua vazio quando preenchido com valor não nmérico', function(){
+cy.get('#phone').type('abcdefg').should('have.value', '')
+
+}
+
+)
+
   })
   
