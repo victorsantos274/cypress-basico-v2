d96e58bf85cc1d2397a4d10b4b9ae656b72d1507
victorsantos274 <victorviolinista274@gmail.com>
2023-11-13 22:42:45 -0300

    VALIDATION MADE SEVERAL TIMES, IMPORTANT TO VALIDATE STABILITY

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,20 @@
+
+
 it('testa a página política de privacidade de forma independente ', function() {
 cy.visit('./src/privacy.html')
+
+
+Cypress._.times(5, function(){
+
+    it('testa a página política de privacidade de forma independente ', function() {
+        cy.visit('./src/privacy.html')
+        
+
+        
+})
+
+})
+
+
 
 })
